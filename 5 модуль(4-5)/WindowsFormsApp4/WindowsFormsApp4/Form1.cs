7e44b3f11d0ae1ace673368f7520ce46f697f86d
kocurtimur-alt <kocurtimur@gmail.com>
2025-10-20 10:53:04 +0300

    Add files via upload

---
+++ 5 модуль(4-5)/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private Image originalImage;
+        private float zoom = 1.0f; // коэффициент масштабирования
+        private Button btnOpen;
+        private TrackBar zoomBar;
+
+        public Form1()
+        {
+            this.Text = "Простой просмотрщик изображений";
+            this.Width = 800;
+            this.Height = 600;
+            this.DoubleBuffered = true; // убираем мерцание
+
+            // Кнопка "Открыть"
+            btnOpen = new Button();
+            btnOpen.Text = "Открыть изображение";
+            btnOpen.Left = 10;
+            btnOpen.Top = 10;
+            btnOpen.Click += BtnOpen_Click;
+            this.Controls.Add(btnOpen);
+
+            // Ползунок зума
+            zoomBar = new TrackBar();
+            zoomBar.Minimum = 1;
+            zoomBar.Maximum = 20;
+            zoomBar.Value = 10;
+            zoomBar.TickFrequency = 1;
+            zoomBar.Left = 180;
+            zoomBar.Width = 200;
+            zoomBar.Top = 10;
+            zoomBar.Scroll += ZoomBar_Scroll;
+            this.Controls.Add(zoomBar);
+
+            this.Paint += Form1_Paint;
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Изображения|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    originalImage = Image.FromFile(ofd.FileName);
+                    this.Invalidate(); // перерисовать окно
+                }
+            }
+        }
+
+        private void ZoomBar_Scroll(object sender, EventArgs e)
+        {
+            zoom = zoomBar.Value / 10f;
+            this.Invalidate(); // обновить изображение
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (originalImage == null) return;
+
+            int newWidth = (int)(originalImage.Width * zoom);
+            int newHeight = (int)(originalImage.Height * zoom);
+
+            // Рисуем изображение по центру окна
+            int x = (this.ClientSize.Width - newWidth) / 2;
+            int y = (this.ClientSize.Height - newHeight) / 2;
+
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            e.Graphics.DrawImage(originalImage, x, y, newWidth, newHeight);
+        }
+             private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
+
---
+++ 5 модуль(4-5)/WpfApp5/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        private double _result;
+        private string _operation;
+        private bool _isOperationPerformed;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (_isOperationPerformed)
+            {
+                ResultTextBox.Text = button.Content.ToString();
+                _isOperationPerformed = false;
+            }
+            else
+            {
+                ResultTextBox.Text += button.Content.ToString();
+            }
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (double.TryParse(ResultTextBox.Text, out _result))
+            {
+                _operation = button.Content.ToString();
+                _isOperationPerformed = true;
+            }
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            double secondOperand;
+
+            if (double.TryParse(ResultTextBox.Text, out secondOperand))
+            {
+                switch (_operation)
+                {
+                    case "+":
+                        _result += secondOperand;
+                        break;
+                    case "-":
+                        _result -= secondOperand;
+                        break;
+                    case "*":
+                        _result *= secondOperand;
+                        break;
+                    case "/":
+                        if (secondOperand != 0)
+                            _result /= secondOperand;
+                        else
+                            ResultTextBox.Text = "Error";
+                        break;
+                }
+                ResultTextBox.Text = _result.ToString();
+                _operation = string.Empty;
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ResultTextBox.Text = string.Empty;
+            _result = 0;
+            _operation = string.Empty;
+        }
+    }
+}
